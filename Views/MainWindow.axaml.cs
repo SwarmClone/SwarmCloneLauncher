5f121441cf25e190bc55959fa22c905d5624edab
郝某人BH <1218271192@qq.com>
2025-08-28 15:10:23 +0800

    创建项目

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SwarmCloneLauncher.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
