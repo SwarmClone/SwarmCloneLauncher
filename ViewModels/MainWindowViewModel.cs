5eb7ef452ec6cb18831ca75835766cc6f9892072
郝某人BH <1218271192@qq.com>
2025-08-28 18:21:03 +0800

    创建侧边栏，添加部分页面

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,69 @@
-﻿namespace SwarmCloneLauncher.ViewModels
+﻿using System;
+using System.Collections.ObjectModel;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace SwarmCloneLauncher.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
 {
-    public partial class MainWindowViewModel : ViewModelBase
+    [ObservableProperty] 
+    private ViewModelBase _currentPage = new HomeViewModel();
+
+    [ObservableProperty] 
+    private bool _isPaneOpen = true;
+
+    [ObservableProperty] 
+    private ListItemTemplate? _selectedListItem;
+
+    public ObservableCollection<ListItemTemplate> Items { get; } = new()
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        new ListItemTemplate(typeof(HomeViewModel), "HomeIcon", "主页"),
+        new ListItemTemplate(typeof(SettingsViewModel), "SettingsIcon", "设置")
+    };
+
+    public MainWindowViewModel()
+    {
+        // 初始化选中项为Home
+        SelectedListItem = Items[0];
     }
+
+    partial void OnSelectedListItemChanged(ListItemTemplate? value)
+    {
+        if (value is null) return;
+        
+        // 只有当当前页面不是目标类型时才创建新实例
+        if (CurrentPage?.GetType() != value.ModelType)
+        {
+            var instance = Activator.CreateInstance(value.ModelType);
+            if (instance is ViewModelBase viewModel)
+            {
+                CurrentPage = viewModel;
+            }
+        }
+    }
+
+    [RelayCommand]
+    private void TriggerPane()
+    {
+        IsPaneOpen = !IsPaneOpen;
+    }
+}
+
+public class ListItemTemplate
+{
+    public ListItemTemplate(Type type, string iconKey, string name)
+    {
+        ModelType = type;
+        Label = name;
+        Application.Current!.TryFindResource(iconKey, out var res);
+        ListItemIcon = (StreamGeometry)res!;
+    }
+    
+    public string Label { get; }
+    public Type ModelType { get; }
+    public StreamGeometry ListItemIcon { get; }
 }
---
+++ Views/ManagementView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SwarmCloneLauncher.Views;
+
+public partial class ManagementView : UserControl
+{
+    public ManagementView()
+    {
+        InitializeComponent();
+    }
+}
